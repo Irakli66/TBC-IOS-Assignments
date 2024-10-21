555abe098bd63c54edb66f77b6212a75baa9d51c
Irakli <99668662+Irakli66@users.noreply.github.com>
2024-10-21 14:13:44 +0400

    finish assignment-17

---
+++ Assignment-17/Assignment-17/Assignment-17/PlanetDetailsViewController.swift
@@ -0,0 +1,201 @@
+//
+//  PlanetDetailsViewController.swift
+//  Assignment-17
+//
+//  Created by irakli kharshiladze on 20.10.24.
+//
+
+import UIKit
+
+struct Planet {
+    let image: UIImage
+    let name: String
+    let area: Int
+    let temperature: Int
+    let mass: Int
+    var favorite: Bool = false
+}
+
+
+final class PlanetDetailsViewController: UIViewController {
+    var planet: Planet? = nil
+    
+    private let planetNameNavigationFavoriteStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .horizontal
+        stackView.distribution = .equalSpacing
+        
+        return stackView
+    }()
+    
+    private let navigationButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setImage(UIImage(systemName: "chevron.left"), for: .normal)
+        button.tintColor = UIColor(red: 179/255, green: 68/255, blue: 22/255, alpha: 1)
+        
+        return button
+    }()
+    
+    private let planetName: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.numberOfLines = 0
+        label.textColor = .white
+        label.font = UIFont.systemFont(ofSize: 36, weight: .bold)
+        
+        return label
+    }()
+    
+    private let favoriteButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setImage(UIImage(systemName: "star.fill"), for: .normal)
+        button.tintColor = .white
+        
+        return button
+    }()
+    
+    private let planetImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit
+        
+        return imageView
+    }()
+    
+    private let planetDetailsTableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return tableView
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+    
+    var addFavorites: (()->Void)?
+
+    private func setupUI() {
+        view.backgroundColor = UIColor(red: 33/255, green: 13/255, blue: 4/255, alpha: 1)
+        setupPlanetNameNavigationFavoriteStackView()
+        setupPlanetImageView()
+        setupPlanetDetailsTableView()
+    }
+    
+    private func setupPlanetNameNavigationFavoriteStackView() {
+        view.addSubview(planetNameNavigationFavoriteStackView)
+        
+        NSLayoutConstraint.activate([
+            planetNameNavigationFavoriteStackView.topAnchor.constraint(equalTo: view.topAnchor, constant: 55),
+            planetNameNavigationFavoriteStackView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 30),
+            planetNameNavigationFavoriteStackView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -30),
+            
+        ])
+        setupNavigationButton()
+        
+        planetNameNavigationFavoriteStackView.addArrangedSubview(planetName)
+        planetName.text = planet?.name
+        
+        setupFavoriteButton()
+        
+    }
+    
+    private func setupNavigationButton() {
+        planetNameNavigationFavoriteStackView.addArrangedSubview(navigationButton)
+        
+        navigationButton.addAction(UIAction(handler: { [weak self] action in
+            self?.navigationController?.popViewController(animated: true)
+        }), for: .touchUpInside)
+    }
+    
+    private func setupFavoriteButton() {
+        planetNameNavigationFavoriteStackView.addArrangedSubview(favoriteButton)
+        
+        favoriteButton.tintColor = planet?.favorite ?? false ? .yellow : .white
+        
+        favoriteButton.addAction(UIAction(handler: {[weak self] action in
+            self?.buttonAction()
+        }), for: .touchUpInside)
+    }
+    
+    private func updateFavoriteButtonUI() {
+        favoriteButton.tintColor = planet?.favorite ?? false ? .yellow : .white
+    }
+    
+    func buttonAction() {
+        addFavorites?()
+        planet?.favorite.toggle()
+        updateFavoriteButtonUI()
+    }
+    
+    private func setupPlanetImageView() {
+        view.addSubview(planetImageView)
+        
+        NSLayoutConstraint.activate([
+            planetImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            planetImageView.topAnchor.constraint(equalTo: planetNameNavigationFavoriteStackView.bottomAnchor, constant: 85),
+            planetImageView.heightAnchor.constraint(equalTo: planetImageView.widthAnchor),
+            planetImageView.heightAnchor.constraint(equalToConstant: 280)
+        ])
+        
+        planetImageView.image = planet?.image
+    }
+    
+    private func setupPlanetDetailsTableView() {
+        view.addSubview(planetDetailsTableView)
+        
+        NSLayoutConstraint.activate([
+            planetDetailsTableView.topAnchor.constraint(equalTo: planetImageView.bottomAnchor, constant: 80),
+            planetDetailsTableView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20),
+            planetDetailsTableView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20),
+            planetDetailsTableView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -40)
+        ])
+        
+        planetDetailsTableView.backgroundColor = .none
+        planetDetailsTableView.dataSource = self
+        planetDetailsTableView.delegate = self
+        planetDetailsTableView.register(PlanetDetailsTableViewCell.self, forCellReuseIdentifier: "PlanetDetailsTableViewCell")
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: animated)
+    }
+}
+
+extension PlanetDetailsViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        [planet?.area, planet?.temperature, planet?.mass].count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "PlanetDetailsTableViewCell", for: indexPath) as? PlanetDetailsTableViewCell {
+            let planetDetails = ["\(planet?.area ?? 0) km2", "\(planet?.temperature ?? 0)℃", "\(planet?.mass ?? 0) kg"]
+            let currentStat = planetDetails[indexPath.row]
+            
+            cell.updateUI(title: "\(indexPath.row == 0 ? "Area" : indexPath.row == 1 ? "Temperature" : "Mass")", stat: currentStat)
+            
+            return cell
+        } else {
+            return UITableViewCell()
+        }
+    }
+    
+    
+}
+
+extension PlanetDetailsViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        80
+    }
+}
---
+++ Assignment-17/Assignment-17/Assignment-17/PlanetsViewController.swift
@@ -0,0 +1,127 @@
+//
+//  ViewController.swift
+//  Assignment-17
+//
+//  Created by irakli kharshiladze on 20.10.24.
+//
+
+import UIKit
+
+final class PlanetsViewController: UIViewController {
+     private var planetsList: [Planet] = [
+        Planet(image: UIImage(named: "Mercury") ?? UIImage(), name: "Mercury", area: 926725476, temperature: -8, mass: 4734653),
+        Planet(image: UIImage(named: "Venus") ?? UIImage(), name: "Venus", area: 623725476, temperature: -18, mass: 7734653),
+        Planet(image: UIImage(named: "Earth") ?? UIImage(), name: "Earth", area: 475167254, temperature: 35, mass: 2734653),
+        Planet(image: UIImage(named: "Mars") ?? UIImage(), name: "Mars", area: 672547623, temperature: 18, mass: 3434653),
+        Planet(image: UIImage(named: "Jupiter") ?? UIImage(), name: "Jupiter", area: 3326725476, temperature: -6, mass: 12534653),
+        Planet(image: UIImage(named: "Uranus") ?? UIImage(), name: "Uranus", area: 4356725476, temperature: 25, mass: 9834653)
+    ]
+    
+    private let pageTitle: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Planets"
+        label.font = UIFont.systemFont(ofSize: 36, weight: .bold)
+        label.textColor = .white
+        
+        return label
+    }()
+    
+    private let planetsCollectionView: UICollectionView = {
+        let collectionView: UICollectionView
+        let collectionLayout = UICollectionViewFlowLayout()
+        collectionLayout.scrollDirection = .vertical
+        collectionLayout.itemSize = CGSize(width: 165, height: 300)
+        
+        collectionView = UICollectionView(frame: CGRect(x: 20, y: 20, width: 100, height: 100), collectionViewLayout: collectionLayout)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.backgroundColor = .none
+        
+        return collectionView
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+    
+    private func setupUI() {
+        view.backgroundColor = UIColor(red: 33/255, green: 13/255, blue: 4/255, alpha: 1)
+        setupPageTitle()
+        setupPlanetCollectionView()
+    }
+    
+    private func setupPageTitle() {
+        view.addSubview(pageTitle)
+        
+        pageTitle.topAnchor.constraint(equalTo: view.topAnchor, constant: 64).isActive = true
+        pageTitle.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+    }
+    
+    private func setupPlanetCollectionView() {
+        view.addSubview(planetsCollectionView)
+        
+        NSLayoutConstraint.activate([
+            planetsCollectionView.topAnchor.constraint(equalTo: pageTitle.bottomAnchor, constant: 56),
+            planetsCollectionView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 15),
+            planetsCollectionView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -15),
+            planetsCollectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -44)
+        ])
+        
+        planetsCollectionView.dataSource = self
+        planetsCollectionView.register(PlanetsCollectionViewCell.self, forCellWithReuseIdentifier: "PlanetsCollectionViewCell")
+        planetsCollectionView.delegate = self
+    }
+
+}
+
+extension PlanetsViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        planetsList.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PlanetsCollectionViewCell", for: indexPath) as? PlanetsCollectionViewCell {
+            let currentPlanet = planetsList[indexPath.row]
+            
+            cell.updateUI(with: currentPlanet)
+            cell.delegate = self
+            return cell
+        } else {
+            return  UICollectionViewCell()
+        }
+    }
+}
+
+extension PlanetsViewController: PlanetCellDelegate {
+    func addToFavorites(cell: PlanetsCollectionViewCell) {
+        if let index = planetsCollectionView.indexPath(for: cell) {
+            let currentIndex = index.row
+            
+            planetsList[currentIndex].favorite.toggle()
+            planetsList.sort { $0.favorite && !$1.favorite }
+            planetsCollectionView.reloadData()
+            
+        } else {
+            return printContent("empty")
+        }
+        
+    }
+    
+}
+
+extension PlanetsViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let currentPlanet = planetsList[indexPath.row]
+        
+        let vc = PlanetDetailsViewController()
+        vc.addFavorites = { [weak self] in
+            self?.planetsList[indexPath.row].favorite.toggle()
+            self?.planetsList.sort { $0.favorite && !$1.favorite }
+            self?.planetsCollectionView.reloadData()
+        }
+        vc.planet = currentPlanet
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
+}
+
