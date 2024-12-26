1b6792a9be8c83b5306df111646f6489e2161386
Irakli <99668662+Irakli66@users.noreply.github.com>
2024-12-27 00:31:38 +0400

    inittial commit

---
+++ Assignment-35/Emoji-Riddle/Emoji-Riddle/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Emoji-Riddle
+//
+//  Created by irakli kharshiladze on 27.12.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
